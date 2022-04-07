29269b5035384167ed51a54cf125264e75f279bf
MuchenSun <sunmch15@gmail.com>
2022-04-07 10:49:24 -0500

    backup before wrapping reader

--- img_rename.py
+++ img_rename.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from tqdm import tqdm
+import os
 
 
 # read in raw data
@@ -141,54 +142,11 @@ boundary_left = np.array(boundary_left)
 boundary_right = np.array(boundary_right)
 
 # start visualization
-fig, ax = plt.subplots(1, 2, figsize=(15.5, 7.5), gridspec_kw={'width_ratios': [1.5, 1]}, tight_layout=True)
+fig, ax = plt.subplots(1, 2, figsize=(14, 7), gridspec_kw={'width_ratios': [1.38, 1]}, tight_layout=True)
 
 traj_lag = 40
 traj_ahead = 30
+file_idx = 0
 for frame in tqdm(full_frame_list):
-    ax[0].cla()
-    frame_img = cv2.imread("./ewap_dataset/seq_eth/frames/frame{0:08}.png".format(frame))
-    ax[0].imshow(frame_img[...,::-1])
-    ax[0].set_aspect("equal")
-    ax[0].xaxis.set_major_locator(plt.NullLocator())
-    ax[0].yaxis.set_major_locator(plt.NullLocator())
-    
-    ax[1].cla()
-    ax[1].set_xlim(-6.0, 18.0)
-    ax[1].set_ylim(15.0, -10.0)
-    ax[1].set_aspect('equal')
-    ax[1].set_title("Frame: {}\n(Dots: past path; Solid line: future path)".format(frame), fontsize=20)
-    ax[1].plot(boundary_left[:,1], boundary_left[:,0], linestyle="-", linewidth=5, color="k")
-    ax[1].plot(boundary_right[:,1], boundary_right[:,0], linestyle="-", linewidth=5, color="k")
-    ax[1].plot(destinations[:,1], destinations[:,0], linestyle="", marker="X", markersize=20, color="k")
-    for axis in ['top', 'bottom', 'left', 'right']:
-        ax[1].spines[axis].set_linewidth(5)
-        ax[1].spines[axis].set_zorder(0)
-    ax[1].tick_params(axis='x', labelsize=20, width=5., length=7.)
-    ax[1].tick_params(axis='y', labelsize=20, width=5., length=7.)
-
-    if frame not in frame_dict:
-        print("missing frame: ", frame)
-        continue
-
-    for ped in frame_dict[frame]["pedestrians"]:
-        state = frame_dict[frame][ped]
-        ax[1].plot(state[1], state[0], linestyle='', marker='o', markersize=15, color='C'+str(ped), label="Ped {}".format(ped))
-
-        curr_frame_idx = np.where(frame == ped_dict[ped]["frames"])[0][0]
-        
-        if curr_frame_idx < traj_lag:
-            traj = ped_dict[ped]["traj"][:curr_frame_idx]
-        else:
-            traj = ped_dict[ped]["traj"][curr_frame_idx-traj_lag:curr_frame_idx]
-        ax[1].plot(traj[:,1], traj[:,0], linestyle=":", linewidth=5, color='C'+str(ped))
-
-        if curr_frame_idx + traj_ahead < len(ped_dict[ped]["frames"]):
-            plan = ped_dict[ped]["traj"][curr_frame_idx: curr_frame_idx+traj_ahead]
-        else:
-            plan = ped_dict[ped]["traj"][curr_frame_idx:]
-        ax[1].plot(plan[:,1], plan[:,0], linestyle="-", linewidth=5, color='C'+str(ped))
-    ax[1].legend(fontsize=10, loc=4, markerscale=0.5, borderpad=0.3) 
-
-    plt.pause(0.01)
-    plt.savefig("./ewap_dataset/seq_eth/img/eth_parallel_frame{0:08}.png".format(frame))
+    os.rename("./ewap_dataset/seq_eth/img_rename/eth_parallel_frame{0:08}.png".format(frame), "./ewap_dataset/seq_eth/img_rename/eth_parallel_frame{0:08}.png".format(file_idx))
+    file_idx += 1
