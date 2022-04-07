9d19e1a68b6a8907356fe56eb5b17852762898bd
MuchenSun <sunmch15@gmail.com>
2022-04-07 17:05:04 -0500

    update

--- analyze_peds.py
+++ analyze_peds.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 eth = reader()
 traj, frames = eth.get_ped_traj(8)
 
-fig, ax = plt.subplots(1, 1, tight_layout=True, figsize=(7, 7))
-for i in range(len(traj)):
-    eth.config_ax(ax)
-    ax.plot(traj[i,1], traj[i,0], linestyle="", marker="o", markersize=5, color="C0")
+fig1, ax1 = plt.subplots(1, 1, tight_layout=True, figsize=(7, 7))
+eth.config_ax(ax1)
+ax1.plot(traj[:,1], traj[:,0], linestyle="-", linewidth=5, color="C0")
 
-    plt.pause(0.01)
-
+fig2, ax2 = plt.subplots(1, 1, tight_layout=True)
+ax2.plot(frames*0.04, traj[:,0], color="C0", label="y")
+ax2.plot(frames*0.04, traj[:,1], color="C1", label="x")
+ax2.legend()
 plt.show()
-plt.close()
+
