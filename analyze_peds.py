dc34cac4471a425395f54b695091e5359b9e1856
MuchenSun <sunmch15@gmail.com>
2022-05-23 18:44:54 -0500

    update

--- analyze_peds.py
+++ analyze_peds.py
@@ -1,16 +1,47 @@
 from reader import reader
-import matplotlib.pyplot as plt
+import numpy as np
+
 
 eth = reader()
-traj, frames = eth.get_ped_traj(8)
 
-fig1, ax1 = plt.subplots(1, 1, tight_layout=True, figsize=(7, 7))
-eth.config_ax(ax1)
-ax1.plot(traj[:,1], traj[:,0], linestyle="-", linewidth=5, color="C0")
+def get_count(thred):
+    count = 0
 
-fig2, ax2 = plt.subplots(1, 1, tight_layout=True)
-ax2.plot(frames*0.04, traj[:,0], color="C0", label="y")
-ax2.plot(frames*0.04, traj[:,1], color="C1", label="x")
-ax2.legend()
+    for ped in eth.ped_idx_all:
+        # print('ped: ', ped)
+        frames = eth.ped_dict[ped]["frames"]
+        flag = 0
+        for frame in frames:
+            ped_pos = eth.ped_dict[ped][frame]
+            
+            neighbors = eth.frame_dict[frame]["pedestrians"]
+            # print(neighbors)
+            for nb in neighbors:
+                if nb == ped:
+                    continue
+                nb_pos = eth.ped_dict[nb][frame]
+                
+                dist = np.linalg.norm(ped_pos[0:2] - nb_pos[0:2])
+                if dist <= thred:
+                    count += 1
+                    flag = 1
+                    break
+            if flag == 1:
+                break
+    
+    return count
+
+thred_list = np.arange(0.1, 1.1, 0.1)
+count_list = []
+for thred in thred_list:
+    count = get_count(thred)
+    count_list.append(count)
+    print("{}: {}".format(thred, count))
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 5))
+ax.plot(thred_list, count_list, "-*")
+
 plt.show()
-
+plt.close()
